4176d41ec32f8842289db4c0f5930383d0971675
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-02 14:23:52 -0700

    got index and show page working as well as post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    @user = User.find_by_id params[:id]
+    render :show
+  end
+
 end
